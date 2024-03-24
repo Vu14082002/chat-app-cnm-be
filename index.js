376cd861e0f9f01bdf5d3248502e5e1c30d18db6
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2024-03-24 11:01:23 +0700

    fix(Socket): Change origin and socket.emit

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const { Server, Socket } = require('socket.io');
 // ----------------------------------------------------------
 const PORT = process.env.PORT || 4000;
 const server = app.listen(PORT, () => {
-     logger.info(`server is running in PORT: ${PORT}`);
+    logger.info(`server is running in PORT: ${PORT}`);
 });
 // socket io
 const io = new Server(server, {
-     pingTimeout: 60000,
-     cors: {
-          origin: '',
-     },
+    pingTimeout: 60000,
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+    },
 });
 io.on('connection', (socket) => {
-     logger.info('socket io connect success');
-     socketServer(socket, io);
+    logger.info('socket io connect success');
+    socketServer(socket, io);
 });
--- src/Socket/socker.js
+++ src/Socket/socker.js
@@ -1,31 +1,31 @@
 const socketServer = (socket, io) => {
-     let userOnline = [];
-     socket.on('online', (userId) => {
-          socket.join(userId);
-          checkOnline = userOnline.some((u) => u.userId === userId);
-          if (!checkOnline) {
-               userOnline.push({ userId, socketId: socket.id });
-          }
-          io.emet('usersOnline', userOnline);
-     });
-     socket.on('disconnect', () => {
-          userOnline = userOnline.filter((u) => u.socketId !== socket.id);
-          io.emet('usersOnline', userOnline);
-     });
-     socket.on('openconversation', (conversationId) => {
-          socket.join(conversationId);
-     });
-     socket.on('sendMessage', (message) => {
-          const conversation = message.conversation;
-          if (!conversation) {
-               return;
-          }
-          conversation.users.forEach((element) => {
-               if (element._id === message.sender._id) {
-                    return;
-               }
-               socket.in(element._id).emet('recivedMessage', message);
-          });
-     });
+    let userOnline = [];
+    socket.on('online', (userId) => {
+        socket.join(userId);
+        checkOnline = userOnline.some((u) => u.userId === userId);
+        if (!checkOnline) {
+            userOnline.push({ userId, socketId: socket.id });
+        }
+        io.emit('usersOnline', userOnline);
+    });
+    socket.on('disconnect', () => {
+        userOnline = userOnline.filter((u) => u.socketId !== socket.id);
+        io.emit('usersOnline', userOnline);
+    });
+    socket.on('openConversation', (conversationId) => {
+        socket.join(conversationId);
+    });
+    socket.on('sendMessage', (message) => {
+        const conversation = message.conversation;
+        if (!conversation) {
+            return;
+        }
+        conversation.users.forEach((element) => {
+            if (element._id === message.sender._id) {
+                return;
+            }
+            socket.in(element._id).emit('receivedMessage', message);
+        });
+    });
 };
 module.exports = { socketServer };
