1a83fc2b4a469a7fa06a81e04c62920f20d082b6
Vu20020814 <nguyenvanvu20020814@gmail.com>
2024-03-26 17:32:22 +0700

    updaet socket and comment

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 const { socketServer } = require('./src/Socket/socker');
 const { app } = require('./src/app');
 const logger = require('./src/logger');
-const { Server, Socket } = require('socket.io');
+const { Server } = require('socket.io');
 
 // ----------------------------------------------------------
 const PORT = process.env.PORT || 4000;
 const server = app.listen(PORT, () => {
-     logger.info(`server is running in PORT: ${PORT}`);
+    logger.info(`server is running in PORT: ${PORT}`);
 });
-// socket io
-const io = new Server(server, {
-     pingTimeout: 60000,
-     cors: {
-          origin: '*',
-          methods: ['GET', 'POST'],
-     },
-});
-io.on('connection', (socket) => {
-     logger.info('socket io connect success');
-     socketServer(socket, io);
+// const io = new Server(server, {
+//     pingTimeout: 60000,
+// });
+// io.on('connection', (socket) => {
+//     logger.info('Co nguoi ket noi', socket.id);
+//     socketServer(socket, io);
+// });
+const io = require('socket.io')(server);
+io.on('connection', client => {
+    console.log("CO nguoi ket noi", client.id);
+    socketServer(client, io)
 });
--- src/Socket/socker.js
+++ src/Socket/socker.js
@@ -1,33 +1,47 @@
+let userOnline = [];
 const socketServer = (socket, io) => {
-     let userOnline = [];
-     socket.on('online', (userId) => {
-          console.log(`mesage`, userId);
-          socket.join(userId);
-          checkOnline = userOnline.some((u) => u.userId === userId);
-          if (!checkOnline) {
-               userOnline.push({ userId, socketId: socket.id });
-          }
-          io.emit('usersOnline', userOnline);
-     });
-     socket.on('disconnect', () => {
-          userOnline = userOnline.filter((u) => u.socketId !== socket.id);
-          io.emit('usersOnline', userOnline);
-     });
-     socket.on('openConversation', (conversationId) => {
-          console.log(conversationId);
-          socket.join(conversationId);
-     });
-     socket.on('sendMessage', (message) => {
-          const conversation = message.conversation;
-          if (!conversation) {
-               return;
-          }
-          conversation.users.forEach((element) => {
-               if (element._id === message.sender._id) {
-                    return;
-               }
-               socket.in(element._id).emit('receivedMessage', message);
-          });
-     });
+  socket.on('online', (userId) => {
+    socket.join(userId);
+    // Kiểm tra xem người dùng đã tồn tại trong danh sách hay chưa
+    const existingUserIndex = userOnline.findIndex((u) => u.userId === userId);
+    // Nếu người dùng chưa tồn tại, thêm vào danh sách
+    if (existingUserIndex === -1) {
+      userOnline.push({ userId, socketId: socket.id });
+    } else {
+      // Nếu người dùng đã tồn tại, cập nhật lại socketId
+      userOnline[existingUserIndex].socketId = socket.id;
+    }
+    console.log(userOnline);
+    io.emit('usersOnline', userOnline);
+  });
+
+  // Xử lý sự kiện ngắt kết nối
+  socket.on('disconnect', () => {
+    // Lọc ra người dùng đã ngắt kết nối và cập nhật lại danh sách
+    userOnline = userOnline.filter((u) => u.socketId !== socket.id);
+    console.log(userOnline);
+    io.emit('usersOnline', userOnline);
+  });
+  // tạo room socket và join vào
+  socket.on('openConversation', (conversationId) => {
+    socket.join(conversationId);
+    console.log(socket.adapter.rooms);
+  });
+  // gui doi nguyen model Message  de tao lay id join vòa room
+  socket.on('sendMessage', (message) => {
+    const conversation = message.conversation;
+    if (!conversation) {
+      return;
+    }
+    conversation.users.forEach((element) => {
+      // ko gui lai tin nhan cho nguoi da gui
+      if (element._id === message.sender._id) {
+        return;
+      }
+      //  chi nhung nguoi co trong room moi dc nhan chat
+      socket.in(element._id).emit('receivedMessage', message);
+    });
+  });
+  // rời nhóm
 };
 module.exports = { socketServer };
